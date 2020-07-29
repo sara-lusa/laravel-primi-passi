e3ed31bca2bce92ef9d4dbd3d1f61e49725c2de3
Sara <saracorsob@gmail.com>
2020-07-29 16:49:17 +0200

    Creazione file

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+
+    <header>
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+    </header>
+
+  </body>
+</html>
