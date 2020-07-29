d0f1272bd2808b75742dbe5e7448e548281d5e4b
Sara <saracorsob@gmail.com>
2020-07-29 17:41:20 +0200

    Piccole modifiche

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>{{ @yield('title') }}</title>
+  </head>
+  <body>
+
+    <header>
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Corso</a>
+          </li>
+          <li>
+            <a href="#">Dopo il corso</a>
+          </li>
+          <li>
+            <a href="#">Lezione gratuita</a>
+          </li>
+          <li>
+            <a href="#">Assumi</a>
+          </li>
+          <li>
+            <a class="button" href="#">Candidati ora</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  </body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,14 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/privacy', function () {
     return view('privacy');
-});
+})->name('privacy');
